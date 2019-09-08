663e589cf645e12c5d87a4fb45b40a7471b241c7
EdansTech <37985949+EdansTech@users.noreply.github.com>
2019-09-08 13:23:27 +0100

    Added all the new NodeJS Files

---
+++ index.js
@@ -0,0 +1,47 @@
+//     All config is now in the config.json file.
+//     Edan's Tech / 2019
+//     https://egddev.com/spacebot
+const Discord = require('discord.js');
+const request = require('request');
+const { prefix, token, nasaapi } = require('./config.json');
+const client = new Discord.Client();
+
+const requestoptions = {
+        url: `https://api.nasa.gov/planetary/apod?api_key=${nasaapi}`,
+        methord: 'GET',
+        headers: {
+                'Accept': 'application/json',
+                'Accept-Charset': 'utf-8'
+        }
+};
+client.once('ready', () => {
+        console.log('Ready')
+})
+
+client.on('message', message => {
+        if(message.author.bot) return;
+        if(message.content.startsWith(`${prefix}space`)) {
+                request(requestoptions, function(err, res, body) {
+                        let json = JSON.parse(body);
+                        message.channel.send("Title: " + json.title);
+                        message.channel.send("Explanation: " + json.explanation);
+                        message.channel.send(json.url);
+                        message.channel.send("Last updated: " + json.date);
+                        console.log("Image posted was last updated: " + json.date);
+                });
+        }
+        if(message.content.startsWith(`${prefix}help`)) {
+                message.channel.send(`${prefix}space - Displays NASAs random image/video of the day`)
+                message.channel.send(`${prefix}credits - Displays the authors credits`)
+                console.log("Help posted")
+        }
+        if(message.content.startsWith(`${prefix}credits`)) {
+                message.channel.send("Bot made by EdansTech")
+                message.channel.send("https://edanstech.pw / https://egddev.com")
+                message.channel.send("Official Supporter")
+                message.channel.send("https://molepatrol.club")
+                console.log("Credits posted")
+        }
+})
+
+client.login(token);
